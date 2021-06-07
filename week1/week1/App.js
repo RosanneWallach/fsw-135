2b55ac4b5262daf68b8214e407f17ea9837fbf3b
Rosanne Wallach <66342005+RosanneWallach@users.noreply.github.com>
2021-06-06 23:23:07 -0400

    Add files via upload

---
+++ week1/week1/App.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Good Morning, Rosie.");
+
+});
+
+app.listen(3000,() => {
+    console.log("The App is listening on port 3000.")
+
+});
+
